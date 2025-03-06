ae1cff5aba3e3817660ea48bff21aadfc71581cb
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2025-03-07 00:11:51 +0530

    l2

--- m2-s2-2510/2-L2-problems/index.js
+++ m2-s2-2510/2-L2-problems/index.js
@@ -70,3 +70,92 @@ function sortNames(namesArray) {
 }
 
 sortNames(["Charlie", "Alice", "Bob"]);
+
+
+// Q: 3
+// Title:
+// L2 - Binding Method in Constructor Function
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.displayInfo=function() {
+    console.log(this.name,this.age);
+  }
+}
+
+
+const Person1 = new Person("swapnil",45)
+Person1.displayInfo()
+
+
+// Q: 4
+// Title:
+// L2 - Building a Countdown Timer with Both setTimeout and setInterval
+
+function countdowntimer(second) {
+    let time=second;
+    let timestart =setInterval(() => {
+    console.log("Timed reaming",time);
+    time--;
+    if(0 > time){
+      clearInterval(timestart);
+      console.log("Countdown complete!");
+    }
+  }, 1000);
+}
+
+countdowntimer(10);
+
+
+// Q: 5
+// Title:
+// L2 - Building a Library Management System Using Factory Functions
+
+function Book(title,author){
+  return{
+    title,
+    author,
+    details(){
+      console.log(`Title: ${this.title}, Author: ${this.author}`)
+    }
+  };
+}
+
+function createLibrary() {
+  const books = [];
+
+  return {
+    addBook:function(book){
+      books.push(book);
+    },
+    removeBook:function(title){
+      const index=books.findIndex((book)=>book.title==title);
+
+      if(index !== -1){
+        books.splice(index,1);
+      }
+    },
+    listBooks: function(){
+      books.forEach((book)=>book.details());
+    }
+  }
+}
+
+let librarys = createLibrary();
+
+const book1 = Book("To Kill a Mockingbird", "Harper Lee");
+const book2 = Book("1984", "George Orwell");
+
+librarys.addBook(book1);
+librarys.addBook(book2);
+
+librarys.listBooks();
+// Output:
+// Title: To Kill a Mockingbird, Author: Harper Lee
+// Title: 1984, Author: George Orwell
+
+librarys.removeBook("1984");
+librarys.listBooks();
+// Output:
+// Title: To Kill a Mockingbird, Author: Harper Lee
