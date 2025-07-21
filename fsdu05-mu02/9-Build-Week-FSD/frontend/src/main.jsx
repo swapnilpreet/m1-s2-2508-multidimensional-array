85e12d4c999785b9a14b7b89d02b70011c9f2d06
swapnil ramteke <97676520+swapnilpreet@users.noreply.github.com>
2025-07-21 17:35:29 +0530

    added

--- fsdu05-mu02/9-Build-Week-FSD/frontend/src/main.jsx
+++ fsdu05-mu02/9-Build-Week-FSD/frontend/src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store.js';
-import App from './App.jsx';
-import './styles/index.css'; // Global styles
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import store from './redux/store.js'
+import {Provider} from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>,
+  </Provider>
+)
